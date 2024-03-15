80eecbdd064c03948c43f97fb3bea786f687a987
rRain <rrainpath@gmail.com>
2024-03-15 21:31:30 +0800

    Added PWA

---
+++ vite.config.ts
@@ -0,0 +1,64 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+
+
+
+const manifest: VitePWAOptions['manifest'] = {
+  name: 'PWA Inject Manifest',
+  short_name: 'PWA Inject',
+  theme_color: '#ffffff',
+  icons: [
+    {
+      src: 'pwa-192x192.png', // <== don't add slash, for testing
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: '/pwa-512x512.png', // <== don't remove slash, for testing
+      sizes: '512x512',
+      type: 'image/png',
+    },
+    {
+      src: 'pwa-512x512.png', // <== don't add slash, for testing
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable',
+    },
+  ],
+}
+
+
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  devOptions: {
+    // enable PWA in dev mode
+    enabled: true,
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+  srcDir: 'src/service-worker',
+  filename: 'service-worker.ts',
+  strategies: 'injectManifest',
+  injectManifest: {
+    minify: false,
+    enableWorkboxModulesLogs: true,
+  },
+  manifest: manifest,
+  base: '/',
+  includeAssets: ['favicon.svg'],
+}
+
+
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA(pwaOptions),
+  ],
+})
