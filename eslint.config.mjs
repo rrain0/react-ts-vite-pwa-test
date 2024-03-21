21aa58ef24f7933adc2f08732d600dcb5f415809
rrain <rrainpath@gmail.com>
2024-03-21 11:20:21 +0800

    tsconfig.json update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -172,6 +172,6 @@ export default [
   },
   // !!! ignores must be in a standalone object to work globally
   {
-    ignores: ['dist', 'dev-dist', 'eslint.config.mjs'],
+    ignores: ['dist', 'dev-dist'],
   }
 ]
