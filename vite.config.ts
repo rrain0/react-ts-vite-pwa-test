aa1d63ff805c9c778435f7f16627f34c5c2fea45
rrain <rrainpath@gmail.com>
2024-03-22 11:42:34 +0800

    vite plugin pwa exact version

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import checker from 'vite-plugin-checker'
 
 
